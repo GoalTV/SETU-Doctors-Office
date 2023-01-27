81fdd25e151f3d703e6e48eee65b810fef482d80
Janusz Czeropski <janusz@mailapp.red>
2023-01-27 03:16:04 +0000

    Added Header and blank home page

---
+++ components/header_tpl.php
@@ -0,0 +1,51 @@
+<head>
+
+		<!-- Basic -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">	
+
+		<title>COVID-19 Booking | <?php if ($page == "") { echo 'Home Page';} else { echo $page;} ?></title>	
+
+		<meta name="keywords" content="" />
+		<meta name="description" content="">
+		<meta name="author" content="Janusz Czeropski">
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+
+		<!-- Mobile Metas -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+
+		<!-- Web Fonts  -->
+		<link id="googleFonts" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800%7CShadows+Into+dark&display=swap" rel="stylesheet" type="text/css">
+
+		<!-- Boostrap CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+
+        <!-- Boostrap JS -->
+		<link rel="stylesheet" href="js/bootstrap.min.js">
+        
+        <!-- Jquery JS -->
+        <link rel="stylesheet" href="js/jquery-3.6.3.min.js">
+
+	</head>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a class="navbar-brand" href="/home">COVID-19 Booking</a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="/home">Home</a>
+        </li>
+      </ul>
+      <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+  <button type="button" class="btn btn-primary">Sign Up</button>
+  <button type="button" class="btn btn-success">Login</button>
+</div>
+    </div>
+  </div>
+</nav>
